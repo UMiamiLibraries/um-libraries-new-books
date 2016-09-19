116a9ab9ed53a3533f10aba094294cee5e8732a5
abelfacenda <abelfacenda26@gmail.com>
2016-09-19 15:42:39 -0400

    Added pnx file cache

--- bookcover.php
+++ bookcover.php
@@ -1,9 +1,15 @@
 <?php
 require_once (__DIR__ ."/um-book-cover/src/app/BookCover.php");
 
-$bookCover = function($syndetics_client_code, $isbn, $image_size) {
+$bookCover = function($syndetics_client_code, $isbn, $image_size, $update_almaIds, $book_cover_url, $alma_id) {
     $cover = new BookCover($syndetics_client_code, $isbn, $image_size);
-    echo $cover->getCover();
+
+    if($update_almaIds){
+        $cover->update_idCache($alma_id, $isbn, $book_cover_url);
+	}else{
+        echo $cover->getCover();
+    }
 };
 
-$bookCover(htmlspecialchars($_GET['syndetics_client_code']), htmlspecialchars($_GET['isbn']), htmlspecialchars($_GET['image_size']));
+$bookCover(htmlspecialchars($_GET['syndetics_client_code']), htmlspecialchars($_GET['isbn']), htmlspecialchars($_GET['image_size']),
+    htmlspecialchars($_GET['update_almaIds']), htmlspecialchars($_GET['book_cover_url']), htmlspecialchars($_GET['alma_id']));
