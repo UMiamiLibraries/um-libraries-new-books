abff30486ea3ecbcaba01e07311494b6fd06ca18
abelfacenda <abelfacenda26@gmail.com>
2016-09-19 19:23:43 -0400

    added if statement

--- bookcover.php
+++ bookcover.php
@@ -4,7 +4,7 @@ require_once (__DIR__ ."/um-book-cover/src/app/BookCover.php");
 $bookCover = function($syndetics_client_code, $isbn, $image_size, $update_almaIds, $book_cover_url, $alma_id) {
     $cover = new BookCover($syndetics_client_code, $isbn, $image_size);
 
-    if($update_almaIds){
+    if(strcmp($update_almaIds, "true") === 0 ){
         $cover->update_idCache($alma_id, $isbn, $book_cover_url);
 	}else{
         echo $cover->getCover();
