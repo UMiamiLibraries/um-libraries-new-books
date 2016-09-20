8212724267e4776bf1eed7b68d8474c0aaafd1df
abelfacenda <abelfacenda26@gmail.com>
2016-09-20 09:39:24 -0400

    testing

--- bookcover.php
+++ bookcover.php
@@ -3,6 +3,7 @@ require_once (__DIR__ ."/um-book-cover/src/app/BookCover.php");
 
 $bookCover = function($syndetics_client_code, $isbn, $image_size, $googleBooksKey) {
     $cover = new BookCover($syndetics_client_code, $isbn, $image_size, $googleBooksKey);
+    echo $googleBooksKey;
     echo $cover->getCover();
 };
 
