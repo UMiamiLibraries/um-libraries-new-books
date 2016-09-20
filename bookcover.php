3513e373cb14ffefa178c945fd271e640e1052e1
abelfacenda <abelfacenda26@gmail.com>
2016-09-20 01:05:18 -0400

    improved performance

--- bookcover.php
+++ bookcover.php
@@ -1,9 +1,9 @@
 <?php
 require_once (__DIR__ ."/um-book-cover/src/app/BookCover.php");
 
-$bookCover = function($syndetics_client_code, $isbn, $image_size) {
-    $cover = new BookCover($syndetics_client_code, $isbn, $image_size);
+$bookCover = function($syndetics_client_code, $isbn, $image_size, $googleBooksKey) {
+    $cover = new BookCover($syndetics_client_code, $isbn, $image_size, $googleBooksKey);
     echo $cover->getCover();
 };
 
-$bookCover(htmlspecialchars($_GET['syndetics_client_code']), htmlspecialchars($_GET['isbn']), htmlspecialchars($_GET['image_size']));
+$bookCover(htmlspecialchars($_GET['syndetics_client_code']), htmlspecialchars($_GET['isbn']), htmlspecialchars($_GET['image_size']), htmlspecialchars($_GET['googleBooksKey']));
--- updatealmacache.php
+++ updatealmacache.php
@@ -1,18 +1,9 @@
 <?php
 $almaCache = function($isbn, $book_cover_url, $alma_id) {
 
-    $url = 'http://sp.library.miami.edu/external_scripts/newitems/cover_cache/alma_ids.json';
-    $jsonString = file_get_contents($url);
-    $data = json_decode($jsonString, true);
+    $data = array($alma_id => array('book_cover' => "$book_cover_url" ,'isbn' => "$isbn"));
 
-    if (empty($data)){
-        $data = array($alma_id => array('book_cover' => "$book_cover_url" ,'isbn' => "$isbn"));
-    }else{
-        $new_item = array($alma_id => array('book_cover' => "$book_cover_url" ,'isbn' => "$isbn"));
-        $data= array_merge($data, $new_item);
-    }
-
-    $path = $_SERVER["DOCUMENT_ROOT"]."/external_scripts/newitems/cover_cache/alma_ids.json";
+    $path = $_SERVER["DOCUMENT_ROOT"]."/external_scripts/newitems/cover_cache/".$alma_id.".json";
 
      $newJsonString = json_encode($data);
     file_put_contents($path, $newJsonString);
