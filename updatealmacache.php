a5729637c64b40a6b784900c42bb63be6886aa7b
abelfacenda <abelfacenda26@gmail.com>
2016-09-19 21:34:21 -0400

    reading and writing id chache file

--- updatealmacache.php
+++ updatealmacache.php
@@ -1,16 +1,21 @@
 <?php
-require_once (__DIR__ ."/um-book-cover/src/app/BookCover.php");
-
 $almaCache = function($isbn, $book_cover_url, $alma_id) {
 
-        $url = 'http://sp.library.miami.edu/external_scripts/newitems/cover_cache/alma_ids.json';
-        $jsonString = file_get_contents($url);
-        $data = json_decode($jsonString, true);
+    $url = 'http://sp.library.miami.edu/external_scripts/newitems/cover_cache/alma_ids.json';
+    $jsonString = file_get_contents($url);
+    $data = json_decode($jsonString, true);
 
-        $new_item[] = array($alma_id => array('book_cover' => "$book_cover_url" ,'isbn' => "$isbn"));
-        array_push($data, $new_item);
+    if (empty($data)){
+        $data = array($alma_id => array('book_cover' => "$book_cover_url" ,'isbn' => "$isbn"));
+    }else{
+        $new_item = array($alma_id => array('book_cover' => "$book_cover_url" ,'isbn' => "$isbn"));
+        $data= array_merge($data, $new_item);
+    }
 
-        file_put_contents($url, $data);
+    $path = $_SERVER["DOCUMENT_ROOT"]."/external_scripts/newitems/cover_cache/alma_ids.json";
+
+     $newJsonString = json_encode($data);
+    file_put_contents($path, $newJsonString);
 
 };
 
